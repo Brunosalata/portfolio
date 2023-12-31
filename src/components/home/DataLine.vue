af872e97b8cdaab559ac2dca8bf6f6c1270b6acc
Bruno Salata <brunoslima@gmail.com>
2023-12-31 11:27:27 -0300

    tecnologies carousel-slide

---
+++ src/components/home/DataLine.vue
@@ -0,0 +1,79 @@
+<template>
+    <v-row class="fill-height d-flex justify-center align-center">
+        <v-col v-for="n in 5" :key="n" cols="2">
+
+            <v-carousel v-model="models[n - 1]" cycle hide-delimiters :show-arrows="false" v-slot="{ selectedClass }"
+                :interval="2000" height="150">
+                <v-carousel-item v-for="(tool, i) in tools" :class="['ma-0 pa-0', selectedClass]" :key="i">
+                    <v-img :src="i === models[n - 1] % tools.length ? tool.initialImage : tool.src"
+                        @click="redirectTo(tool.href)" style="height: 75%;" class="mt-3" content></v-img>
+                </v-carousel-item>
+            </v-carousel>
+
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            tools: [
+                {
+                    initialImage: 'https://www.cdnlogo.com/logos/v/92/vue-js.svg',
+                    src: 'https://www.cdnlogo.com/logos/v/92/vue-js.svg',
+                    href: 'https://google.com.br'
+                },
+                {
+                    initialImage: 'https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-light-atom.svg',
+                    src: 'https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-light-atom.svg',
+                    href: 'https://vuetifyjs.com/'
+                },
+                {
+                    initialImage: 'https://www.cdnlogo.com/logos/j/2/java.svg',
+                    src: 'https://www.cdnlogo.com/logos/j/2/java.svg',
+                    href: 'https://www.java.com/pt-BR/'
+                },
+                {
+                    initialImage: 'https://www.cdnlogo.com/logos/s/41/sqlite.svg',
+                    src: 'https://www.cdnlogo.com/logos/s/41/sqlite.svg',
+                    href: 'https://google4.com.br'
+                },
+                {
+                    initialImage: 'https://www.cdnlogo.com/logos/v/23/vitejs.svg',
+                    src: 'https://www.cdnlogo.com/logos/v/23/vitejs.svg',
+                    href: 'https://vitejs.dev/'
+                },
+                {
+                    initialImage: 'https://www.cdnlogo.com/logos/s/91/spring.svg',
+                    src: 'https://www.cdnlogo.com/logos/s/91/spring.svg',
+                    href: 'https://google6.com.br'
+                },
+            ],
+            models: [0, 1, 2, 3, 4] // Um modelo diferente para cada coluna
+        };
+    },
+    methods: {
+        redirectTo(link) {
+            window.open(link, '_blank');
+        }
+    }
+};
+</script>
+
+<style scoped>
+.v-img {
+    cursor: pointer;
+    height: 100%;
+    width: 100%;
+}
+
+.v-carousel-item {
+    margin: 0;
+    border-radius: 50%;
+}
+
+.v-carousel-item .hover {
+    scale: 1.02;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,54 @@
 <template>
   <h1 class="mb-2 text-md-h3">Be there from anywhere</h1>
-  <div><Carousel /></div>
+  <div>
+    <Carousel />
+  </div>
   <div class="pa-5 pt-0">
-    <div class="pt-15 pb-15"><Brands /></div>
+    <div class="pt-15 pb-15">
+      <Brands />
+    </div>
     <v-divider></v-divider>
-    <div class="pt-10"><Description /></div>
+    <div class="pt-10">
+      <Description />
+    </div>
   </div>
   <div class="bg-grey-darken-4 d-flex justify-center align-center">
-    <div style="padding-top: 80px; padding-bottom: 80px; width: 90%;">
+    <div style="padding-top: 80px; width: 90%;">
       <div class="text-yellow-darken-1" flex style="padding-left: 2%;">
         <h1>Atributos</h1>
       </div>
-      <div class="pt-10 pb-15"><Skills /></div>
+      <div class="pt-10 pb-15">
+        <Skills />
+      </div>
       <v-divider></v-divider>
+      <div class="pt-15">
+        <div class="mb-10 text-yellow-darken-1 d-flex justify-center align-center">
+          <h1>Tecnologias</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="bg-white">
+    <DataLine />
+  </div>
+
+  <div class="bg-grey-darken-4 d-flex justify-center align-center">
+    <div style="padding-bottom: 80px; width: 90%;">
+      <div class="pt-15 pb-15">
+        <ProjectsView />
+      </div>
     </div>
   </div>
   <SlideGroup />
 </template>
 
 <script lang="ts" setup>
-  import Carousel from '@/components/home/Carousel.vue';
-  import Brands from '@/components/home/Brands.vue';
-  import SlideGroup from '@/components/home/SlideGroup.vue';
-  import Description from '@/components/home/Description.vue';
-  import Skills from '@/components/home/Skills.vue';
+import Carousel from '@/components/home/Carousel.vue';
+import Brands from '@/components/home/Brands.vue';
+import SlideGroup from '@/components/home/SlideGroup.vue';
+import Description from '@/components/home/Description.vue';
+import Skills from '@/components/home/Skills.vue';
+import DataLine from '@/components/home/DataLine.vue';
+import ProjectsView from '@/components/home/ProjectsView.vue';
 </script>
