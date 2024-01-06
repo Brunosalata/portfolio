7d593d65bbbb81fe35aaf15c4ac02394eee6cee6
Bruno Salata <brunoslima@gmail.com>
2024-01-06 20:40:25 -0300

    author local image

--- src/components/about/Description.vue
+++ src/components/about/Description.vue
@@ -6,7 +6,7 @@
           <v-card variant="flat" style="border-radius: 10%;">
             <v-img cover height="auto"
               style="border-radius: 30%; object-fit: cover; object-position: top; filter:grayscale(10%); z-index: 990;"
-              src="https://lh3.googleusercontent.com/pw/ABLVV85pstzfFypq2TmEJaWzr9_hXPoGSALqNTG0-gXyNpcwiY-k89H21MQhteB6fk2kIwhNF6gB4r3nCPbdmeHtiapOTy08GBiqMSHf5vLVXE2__1g9wrH208bwSYL1QbZzUNpNGuSzZNmuJ4FTZb7TupVn_1ZmYqjtuIQi70dpsclpE3wroX_aYxUncMzCnglB2KtbpeureZd2kKy4-fsOwiWbfY6XaQUYSmON02ijIPdKgENgJ_d92yzFzAdlNm68yr8I7zvr_Ov2N95SKhZW2V8ULR1N4aKwSulNp7nhBVK9_uPIl8s-I4e9hkmsEi2GVGE7X0_IJb3DP-gDKvEFQuBmrT5JW7P-9DjzVa3Ap-Y9leLRheO_-T5vknV_QhOGXL7CE5jhsF6kchAx84xVpwR_bBoAngsa7rKsovaYvI3zAlcAHYIkuKTgB3wm1EH3JjrwwYL7f7TMw1FWWo3gvzj0eS6zkB6BUIzFhUW3tQQufceNSLrusDW0v0B2CJ_mOnd9uW73v4C8QDAJb9ax2LWEJDm6y1r956zKELwPZGkZl-VrxEBnWCQDgU9aUjDzM5My__29kR__laQPSN-YI11_t66j77QOjmm-xAWfzI63-Vkw5kUcnLLs3dWhUijwEVrOdNQTZoDeTyIaFnd0nxLybzdxaHbtFuUGGXHcvNAso5J0a1HruJe4jp26E0W7olluN52CAUVfofPFkmbs__9GD0gjHQxtQ3c3DM0ha4EMDXLlb_fvS2QwctxYAOffIHXfvQn_KKhKkYYOYDZfH5wzzMBRwNDl4p5ozeJNCckao4x9r2Pw1vQQhPNrzW2H6ZEMtbq6l0OchpD-o58Zg67q3KVQ-bIIYzP_R3OZEHypn8j_d_Cnw8dIyC8iuiYD9jHErfs=w637-h658-s-no-gm?authuser=0"></v-img>
+              src="@/assets/img/bruno.jpeg"></v-img>
           </v-card>
         </div>
       </v-col>
